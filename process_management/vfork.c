f7b636a69c2ce2aaf0b46fbd25f820116347aa4d
THASHAMSHETTYNIKHITHA <nthashamshetty630@gmail.com>
2023-07-04 22:06:09 +0530

    lsp

---
+++ process_management/vfork.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+	int a;
+	pid_t pid;
+	pid=vfork();
+    if (pid==0)
+	{
+		printf("I am a child process\n");
+		printf("Child process ID : %d \n", getpid());
+	    printf("Child's Parent process ID : %d\n",getppid());
+	//    execl("./fork",argv[0],NULL);
+		 a=19;
+		_exit(0);
+    }  
+    else
+    {
+		printf("I am a parent process\n");
+		printf("Parent process ID : %d \n",getpid());
+		printf("Parent's parent ID(Shell id) : %d\n",getppid());
+ 		printf("a=%d\n",a);	
+    }
+	return 0;
+}
